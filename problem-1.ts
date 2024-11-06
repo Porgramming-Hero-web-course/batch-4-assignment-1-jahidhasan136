741f09e4029764b6adedc366af80756da4309829
jahidhasan136 <jh3495689@gmail.com>
2024-11-06 20:52:31 +0600

    problem 1 done add to the private repo

--- problem-1.ts
+++ problem-1.ts
@@ -1,4 +1,15 @@
 {
-    // set github private repo
-    console.log("hobe everything is fine")
+    //Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+
+    function sumArray(numbers: number[]) {
+        const reduceMethod = numbers.reduce((acc, num) => acc + num, 0)
+        return reduceMethod;
+    }
+
+
+
+
+
+
+    //
 }
