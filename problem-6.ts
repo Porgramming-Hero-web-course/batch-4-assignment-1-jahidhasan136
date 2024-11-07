f4ff2e281f720a7bb31453d249006e6cfa6efb61
jahidhasan136 <jh3495689@gmail.com>
2024-11-07 06:45:07 +0600

    solve six number problem

---
+++ problem-6.ts
@@ -0,0 +1,24 @@
+{
+    //Define an interface Profile with properties name, age, and email. Create a function updateProfile that accepts an object of type Profile and an object of type Partial representing the updates. The function should return the updated profile.
+
+
+    interface Profile {
+        name: string;
+        age: number;
+        email: string;
+    }
+
+    function updateProfile(obj: Profile, updates: Partial<Profile>): Profile {
+        return { ...obj, ...updates };
+    }
+
+
+
+
+
+
+
+
+
+    //
+}
